30681a94aa6b831ce3233fac4deb4aba843bb7f9
Niall Donnelly <niall.donnelly@feedhenry.com>
2016-11-22 09:35:44 +0000

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,95 @@
+'use strict';
+
+module.exports = function(grunt) {
+  require('time-grunt')(grunt);
+  // Project Configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      js: {
+        files: ['Gruntfile.js', 'application.js', 'lib/**/*.js'],
+        options: {
+          livereload: true
+        }
+      },
+      html: {
+        files: ['public/views/**', 'app/views/**'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    nodemon: {
+      dev: {
+        script: 'application.js',
+        options: {
+          args: [],
+          ignore: ['public/**'],
+          ext: 'js,html',
+          nodeArgs: [],
+          delayTime: 1,
+          env: {
+            PORT: 3000
+          },
+          cwd: __dirname
+        }
+      }
+    },
+    concurrent: {
+      serve: ['nodemon', 'watch'],
+      debug: ['node-inspector', 'shell:debug', 'open:debug'],
+      options: {
+        logConcurrentOutput: true
+      }
+    },
+    env : {
+      options : {},
+      // environment variables - see https://github.com/jsoverson/grunt-env for more information
+      local: {
+        FH_USE_LOCAL_DB: true,
+        FH_SERVICE_MAP: function() {
+          /*
+           * Define the mappings for your services here - for local development.
+           * You must provide a mapping for each service you wish to access
+           * This can be a mapping to a locally running instance of the service (for local development)
+           * or a remote instance.
+           */
+          var serviceMap = {
+            'SERVICE_GUID_1': 'http://127.0.0.1:8010',
+            'SERVICE_GUID_2': 'https://host-and-path-to-service'
+          };
+          return JSON.stringify(serviceMap);
+        }
+      }
+    },
+    'node-inspector': {
+      dev: {}
+    },
+    shell: {
+      debug: {
+        options: {
+          stdout: true
+        },
+        command: 'env NODE_PATH=. node --debug-brk application.js'
+      }
+    },
+    open: {
+      debug: {
+        path: 'http://127.0.0.1:8080/debug?port=5858',
+        app: 'Google Chrome'
+      },
+      platoReport: {
+        path: './plato/index.html',
+        app: 'Google Chrome'
+      }
+    }
+  });
+
+  // Load NPM tasks
+  require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+
+
+  grunt.registerTask('serve', ['env:local', 'concurrent:serve']);
+  grunt.registerTask('debug', ['env:local', 'concurrent:debug']);
+  grunt.registerTask('default', ['serve']);
+};
---
+++ application.js
@@ -0,0 +1,32 @@
+var mbaasApi = require('fh-mbaas-api');
+var express = require('express');
+var fhComponentMetrics = require('fh-component-metrics');
+var mbaasExpress = mbaasApi.mbaasExpress();
+var cors = require('cors');
+
+var securableEndpoints;
+securableEndpoints = [];
+
+var app = express();
+
+// Enable CORS for all requests
+app.use(cors());
+
+// Note: the order which we add middleware to Express here is important!
+app.use('/sys', mbaasExpress.sys(securableEndpoints));
+app.use('/mbaas', mbaasExpress.mbaas);
+
+// allow serving of static files from the public directory
+app.use(express.static(__dirname + '/public'));
+
+// Note: important that this is added just before your own Routes
+app.use(mbaasExpress.fhmiddleware());
+
+// Important that this is last!
+app.use(mbaasExpress.errorHandler());
+
+var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
+var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var server = app.listen(port, host, function() {
+  console.log("App started at: " + new Date() + " on port: " + port);
+});
